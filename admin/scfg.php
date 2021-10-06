8ce01704ffdd7596cf467e98a7afc6c68f629436
memojl <memojl08@gmail.com>
2021-10-05 22:31:45 -0500

    update scfg 05.10.2010

--- admin/scfg.php
+++ admin/scfg.php
@@ -1,16 +1,22 @@
 <?php 
 $h_s='phponix.webcindario.com';
 if($_SERVER['HTTP_HOST']==$h_s || $_SERVER['HTTP_HOST']=='www.'.$h_s){
-$db_host = "mysql.webcindario.com";// Localhost
-$db_base = "";      // Nombre de la Base de Datos
-$db_user = "";      // Usuario de la Base de Datos
-$db_pass = "";     	// Password de la Base de Datos 
+    // Localhost Production
+    $db_host = "mysql.webcindario.com";
+    // Database Name      
+    $db_base = ""; 
+    // Database Username     
+    $db_user = "";
+    // Database Password       
+    $db_pass = "";     	
 }else{
-$db_host = "localhost"; // Localhost
-$db_base = "phponix_dev";   // Nombre de la Base de Datos
-$db_user = "root";  // Usuario de la Base de Datos
-$db_pass = "";  // Password de la Base de Datos
+    // Localhost Development
+    $db_host = "localhost";
+    $db_base = "phponix";
+    $db_user = "root"; 
+    $db_pass = "";     	        
 }
+// Settings
 $config = [
     "driver" => "mysql",
     "host" => $db_host,
@@ -20,10 +26,20 @@ $config = [
     "port" => "3306",
     "charset" => "utf8mb4"
 ];
-$DBprefix = "php_";	// Prefijo para las tablas de la Base de datos.
-/*DEFINICION DE VARIABLES PARA PHP7*/
-define('DB_HOST',$db_host);
-define('DB_USER',$db_user);
-define('DB_PASSWORD',$db_pass);
-define('DB_DB',$db_base);
+// Prefix for the database tables.
+$DBprefix = "php_";
+// Project root path		
+$path_root = '';
+// Database name and path for sqlite
+$rut = (basename($_SERVER['PHP_SELF'])=='index.php')?'../':'../../';
+$dbSQLite=$rut.'db/'.$config['database'].'.db';
+
+/*DEFINITION OF VARIABLES FOR PHP7*/
+define('DB_DRIVER',$config['driver']);
+define('DB_HOST',$config['host']);
+define('DB_USER',$config['username']);
+define('DB_PASSWORD',$config['password']);
+define('DB_DB',$config['database']);
+define('DB_PORT',$config['port']);
+define('DB_CHARSET',$config['charset']);
 ?>
