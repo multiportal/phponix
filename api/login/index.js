c77a2522b2972400c1d36e6d04d4ec539a6b7193
memojl <memojl08@gmail.com>
2024-02-11 13:50:04 -0600

    Update Api Login

--- api/login/index.js
+++ api/login/index.js
@@ -12,7 +12,7 @@ const path_url = pathname;
 const URL = window.location.href;
 console.log('URL='+URL);
 
-var proyecto = 'mandragora'; //PROYECTO
+var proyecto = 'phponix'; //PROYECTO
 //console.log('proyecto=' + proyecto);
 var path_root = (host=='localhost')?'MisSitios/' + proyecto + '/':'';
 //console.log('path_root='+path_root);
